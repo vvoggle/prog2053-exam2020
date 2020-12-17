34cd3be4bf9cc98ae778709fc1f711eac42e91af
yans <yans@stud.ntnu.no>
2020-12-17 12:50:18 +0100

    siste siste endring

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -20,11 +20,11 @@ class EditUser extends LitElement {
        <form method="POST">
         <input type="hidden" name="uid" id="uid" value="${this.user.uid}"> 
         <label for="uname">Brukernavn</label>
-          <input type="text" name="uname" id="uname" value="${this.user.uname}"><br>
+          <input type="text" name="uname" id="uname" value="${this.user.uname}" required><br>
         <label for="firstName">Fornavn</label>
-          <input type="text" name="firstName" id="firstName" value="${this.user.firstName}"><br>
+          <input type="text" name="firstName" id="firstName" value="${this.user.firstName}" required><br>
         <label for="lastName">Etternavn</label>
-          <input type="text" name="lastName" id="lastName" value="${this.user.lastName}"><br>
+          <input type="text" name="lastName" id="lastName" value="${this.user.lastName}" required><br>
         <label for="oldpwd">Gamle passord</label>
           <input type="password" name="oldpwd" id="oldPwd"><br>
         <label for="nupwd">Nytt passord</label>
@@ -36,11 +36,10 @@ class EditUser extends LitElement {
   }
 
   updUser(inp){
-      const nuForm = new data(inp.target.form);
       console.log(inp);                 //show form info in console
       fetch('api/updateUser.php', {    //send data to updateUser
         method: 'POST',
-        body: nuForm   //body is the form within the render
+        body: inp.target.form   //body is the form within the render
       }).then(res=>res.json())
         .then(data=>{                 // error check
           if (data.status=='success') {
